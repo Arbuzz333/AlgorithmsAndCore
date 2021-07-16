0959d6574049f0d39f3d0d91ea322f3f57af4974
Arbuzz333 <arbuz333@hotmail.com>
2021-07-16 23:55:14 +0300

    while to for

--- sorting/src/main/kotlin/avahidov/com/sorting/bubble/BubbleSorting.kt
+++ sorting/src/main/kotlin/avahidov/com/sorting/bubble/BubbleSorting.kt
@@ -8,12 +8,10 @@ class BubbleSorting: SortingArray {
     override fun sortFun(original: Array<Int>): Array<Int> {
         val size = original.size
         for (index in original.indices) {
-            var j = index + 1
-            while (j < size) {
+            for (j in index + 1 until size) {
                 if (original[j] < original[index]) {
                     swapArray(original, j, index)
                 }
-                j++
             }
         }
         return original
--- sorting/src/main/kotlin/avahidov/com/sorting/comb/CombSort.kt
+++ sorting/src/main/kotlin/avahidov/com/sorting/comb/CombSort.kt
@@ -11,7 +11,7 @@ class CombSort : SortingArray {
     override fun sortFun(original: Array<Int>): Array<Int> {
         var step: Int = original.size - 1
 
-        while (step > 1) {
+        while (step >= 1) {
             for (i in original.indices) {
                 val j = if (i + step < original.size) i + step else original.size - 1
                 if (original[i] > original[j]) {
@@ -19,16 +19,6 @@ class CombSort : SortingArray {
                 }
             }
             step = (step / factor).toInt()
-        }
-        /*сортировка пузырьком*/
-        for (idx_i in original.indices) {
-            var j = 0
-            while (j + 1 < original.size - idx_i) {
-                if (original[j + 1] < original[j]) {
-                    swapArray(original, j, j + 1)
-                }
-                j++
-            }
         }
         return original
     }
