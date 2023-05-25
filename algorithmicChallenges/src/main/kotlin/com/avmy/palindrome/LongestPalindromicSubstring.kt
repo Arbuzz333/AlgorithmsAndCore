f87450ddd9a7e92bddff0cb5d18789191ec2509c
Arbuzz333 <arbuz333@hotmail.com>
2023-05-25 21:50:11 +0300

    LongestPalindromicSubstring to be better

--- algorithmicChallenges/src/main/kotlin/com/avmy/palindrome/LongestPalindromicSubstring.kt
+++ algorithmicChallenges/src/main/kotlin/com/avmy/palindrome/LongestPalindromicSubstring.kt
@@ -7,61 +7,41 @@ package com.avmy.palindrome
 object LongestPalindromicSubstring {
 
     fun longestPalindromic(s: String): String {
-        if (s.length < 2) return s
-
-        val oddPair = palOdd(s)
-        val oddSub = s.substring(oddPair.second - oddPair.first + 1, oddPair.second + oddPair.first)
-        val evenPair = palEven(s)
-        val evenSub = s.substring(evenPair.second - evenPair.first + 1, evenPair.second + evenPair.first + 1)
-        return if (oddSub.length > evenSub.length) oddSub else evenSub
-    }
-
-    private fun palOdd(s: String): Pair<Int, Int> {
-        var dMax = 0
-        var iMax = 0
+        var dEvenMax = 0
+        var iEvenMax = 0
+        var dOddMax = 0
+        var iOddMax = 0
 
         val n: Int = s.length
-        val d = IntArray(s.length)
-        var right = 0
-        for (index  in 1 until n) {
-
-            while (index - d[index] >= 0 && index + d[index] < n && (index - d[index] == index + d[index] || s[index - d[index]] == s[index + d[index]])) {
-                d[index]++
-                if (d[index] >= dMax) {
-                    dMax = d[index]
-                    iMax = index
-                }
-            }
-            if (index + d[index] - 1 > right) {
-                right = index + d[index] - 1
-            }
-        }
-        return Pair(dMax, iMax)
-    }
-
-    private fun palEven(s: String): Pair<Int, Int> {
-        var dMax = 0
-        var iMax = 0
-
-        val n: Int = s.length
-        val d = IntArray(s.length)
-
-        var right = -1
+        val evenArray = IntArray(n)
+        val oddArray = IntArray(n)
 
         for (index  in 0 until n) {
 
-            while (index - d[index] >= 0 && index + d[index] + 1 < n && s[index - d[index]] == s[index + d[index] + 1]) {
-                d[index]++
-                if (d[index] >= dMax) {
-                    dMax = d[index]
-                    iMax = index
+            while (index - evenArray[index] >= 0 && index + evenArray[index] + 1 < n &&
+                s[index - evenArray[index]] == s[index + evenArray[index] + 1]) {
+
+                evenArray[index]++
+                if (evenArray[index] >= dEvenMax) {
+                    dEvenMax = evenArray[index]
+                    iEvenMax = index
                 }
             }
-            if (index + d[index] > right) {
-                right = index + d[index]
+
+            while (index - oddArray[index] >= 0 && index + oddArray[index] < n &&
+                (oddArray[index] == 0 || s[index - oddArray[index]] == s[index + oddArray[index]])) {
+
+                oddArray[index]++
+                if (oddArray[index] >= dOddMax) {
+                    dOddMax = oddArray[index]
+                    iOddMax = index
+                }
             }
         }
 
-        return Pair(dMax, iMax)
+        val oddSub = s.substring(iOddMax - dOddMax + 1, iOddMax + dOddMax)
+        val evenSub = s.substring(iEvenMax - dEvenMax + 1, iEvenMax + dEvenMax + 1)
+        return if (oddSub.length > evenSub.length) oddSub else evenSub
     }
+
 }
