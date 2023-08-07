59b6826ea67f890ee53bf7de35d4670b60409402
AlexVahidoff <intellect555@outlook.com>
2023-08-07 19:48:32 +0300

    FindTheStringWithLCP findTheString

--- algorithmicChallenges/src/main/kotlin/com/avmy/str/FindTheStringWithLCP.kt
+++ algorithmicChallenges/src/main/kotlin/com/avmy/str/FindTheStringWithLCP.kt
@@ -1,8 +1,5 @@
 package com.avmy.str
 
-import kotlin.math.max
-import kotlin.math.min
-
 
 object FindTheStringWithLCP {
 
@@ -24,12 +21,12 @@ object FindTheStringWithLCP {
         return s
     }
 
-    fun buildFromZ(z : IntArray): String
+    fun buildFromZ(z : IntArray, ch: Char = 'a'): String
     {
         var s = ""
         var prefixLength = 0 // длина префикса, который мы записываем
         var j = 0 // позиция символа в строке, который будем записывать
-        var newCharacter = 'a' // индекс нового символа
+        var newCharacter = ch // индекс нового символа
         for (i in z.indices) {
             // мы не пишем какой-то префикс и не будем писать новый
             if (z[i] == 0 && prefixLength == 0) {
@@ -60,7 +57,8 @@ object FindTheStringWithLCP {
         var left = 0
         var right = 0
         for (i in 1 until n) {
-            zf[i] = max(0, min(right - i, zf[i - left]))
+            val min = if(right - i < zf[i - left]) right - i else zf[i - left]
+            zf[i] = if(0 > min) 0 else min
             while (i + zf[i] < n && s[zf[i]] == s[i + zf[i]]) {
                 zf[i]++
             }
@@ -70,6 +68,54 @@ object FindTheStringWithLCP {
             }
         }
         return zf
+    }
+
+    fun findTheString(lcp: Array<IntArray>): String {
+        if (lcp.size == 1 && lcp[0][0] == 0) return ""
+
+        val result = CharArray(lcp[0].size) { _: Int ->  '#' }
+        result[0] = 'a'
+        var next = 'a'
+        var dNext = 'a'
+        for (ind in lcp.indices) {
+            if (lcp[ind].last() > 1) return ""
+
+            val arrForBuild = lcp[ind].takeLast(lcp.size - ind).toIntArray()
+            arrForBuild.forEachIndexed { index, arr ->
+                if (index > 0 && result[index + ind] == '#' && arr > 0) result[index + ind] = next
+            }
+            if (result[ind] == '#') result[ind] = next
+            next = if (1 < arrForBuild.size) {
+                if (arrForBuild.all { it == 0 }) {
+                    next
+                } else {
+                    if (result[ind + 1] != '#') result[ind + 1] else {
+                        if (dNext == 'z') 'a' else {
+                            dNext += 1
+                            dNext
+                        }
+                    }
+                }
+            } else {
+                next
+            }
+        }
+        var s = String(result)
+
+        val length = s.length
+        if (s.isEmpty()) return ""
+        for (ind in lcp.indices) {
+            val prefStr = if (ind == 0) s else s.takeLast(length - ind) + "#" + s
+            val zArr = zFunction(prefStr)
+            val zArrM = zArr.takeLast(length).toIntArray()
+            zArrM[ind] = length - ind
+            if (zArrM.joinToString() != lcp[ind].joinToString()) {
+                s =  ""
+                break
+            }
+        }
+
+        return s
     }
 
 }
--- algorithmicChallenges/src/test/kotlin/com/avmy/str/FindTheStringWithLCPTest.kt
+++ algorithmicChallenges/src/test/kotlin/com/avmy/str/FindTheStringWithLCPTest.kt
@@ -12,9 +12,21 @@ class FindTheStringWithLCPTest {
         @JvmStatic
         fun pair(): Stream<Arguments> {
             return Stream.of(
-                    Arguments.of(Pair(intArrayOf(0,0,1,0,3,0,1), "abacaba")),
-                    Arguments.of(Pair(intArrayOf(0,0,2,0), "abab")),
-                    Arguments.of(Pair(intArrayOf(4,3,2,1), "")),
+                    Arguments.of(Triple(intArrayOf(0, 0, 1, 0, 3, 0, 1), 'a', "abacaba")),
+                    Arguments.of(Triple(intArrayOf(0, 0, 2, 0), 'a', "abab")),
+                    Arguments.of(Triple(intArrayOf(4, 3, 2, 1), 'a', "")),
+                    Arguments.of(Triple(intArrayOf(0, 0, 0, 0, 0, 1, 2, 0), 'a', "abcdeaab")),
+                    Arguments.of(Triple(intArrayOf(   0, 0, 1, 1, 0, 0, 1), 'b', "bcbbdeb")),
+                    Arguments.of(Triple(intArrayOf(      0, 0, 0, 0, 0, 0), 'c', "cdefgh")),
+                    Arguments.of(Triple(intArrayOf(         0, 1, 0, 0, 1), 'b', "bbcdb")),
+                    Arguments.of(Triple(intArrayOf(            0, 0, 0, 1), 'b', "bcdb")),
+                    Arguments.of(Triple(intArrayOf(               0, 1, 0), 'a', "aab")),
+                    Arguments.of(Triple(intArrayOf(                  0, 0), 'a', "ab")),
+
+                    Arguments.of(Triple(intArrayOf(0, 0, 0, 1, 1, 0, 0, 1), 'b', "bcdbbefb")),
+                    Arguments.of(Triple(intArrayOf(0, 0, 0, 0, 0, 0, 0, 0), 'c', "cdefghij")),
+                    Arguments.of(Triple(intArrayOf(0, 1, 0, 0, 1, 0, 0, 1), 'b', "bbcdbefb")),
+                    Arguments.of(Triple(intArrayOf(0, 1, 0, 1, 4, 0, 0, 1), 'b', "bbcbbbcb")),
             )
         }
 
@@ -26,14 +38,54 @@ class FindTheStringWithLCPTest {
                     Arguments.of(Pair("b#abab", intArrayOf(0, 0, 0, 1, 0, 1))),
             )
         }
+
+        @JvmStatic
+        fun findTheString(): Stream<Arguments> {
+            return Stream.of(
+                    Arguments.of(Pair("abab",
+                            arrayOf(intArrayOf(4, 0, 2, 0), intArrayOf(0, 3, 0, 1), intArrayOf(2, 0, 2, 0), intArrayOf(0, 1, 0, 1)))),
+                    Arguments.of(Pair("aaaa",
+                            arrayOf(intArrayOf(4, 3, 2, 1), intArrayOf(3, 3, 2, 1), intArrayOf(2, 2, 2, 1), intArrayOf(1, 1, 1, 1)))),
+                    Arguments.of(Pair("",
+                            arrayOf(intArrayOf(4, 3, 2, 1), intArrayOf(3, 3, 2, 1), intArrayOf(2, 2, 2, 1), intArrayOf(1, 1, 1, 3)))),
+                    Arguments.of(Pair("",
+                            arrayOf(intArrayOf(2, 2), intArrayOf(1, 1)))),
+                    Arguments.of(Pair("ab",
+                            arrayOf(intArrayOf(2, 0), intArrayOf(0, 1)))),
+                    Arguments.of(Pair("",
+                            arrayOf(intArrayOf(3,2,0), intArrayOf(2,2,1), intArrayOf(1,1,1)))),
+                    Arguments.of(Pair("",
+                            arrayOf(intArrayOf(0)))),
+                    Arguments.of(Pair("abcbbaab",
+                            arrayOf(intArrayOf(8, 0, 0, 0, 0, 1, 2, 0), intArrayOf(0, 7, 0, 1, 1, 0, 0, 1), intArrayOf(0, 0, 6, 0, 0, 0, 0, 0),
+                                    intArrayOf(0, 1, 0, 5, 1, 0, 0, 1), intArrayOf(0, 1, 0, 1, 4, 0, 0, 1),
+                                    intArrayOf(1, 0, 0, 0, 0, 3, 1, 0), intArrayOf(2, 0, 0, 0, 0, 1, 2, 0), intArrayOf(0, 1, 0, 1, 1, 0, 0, 1)))),
+                    Arguments.of(Pair("abacaaaaacbacaa",
+                            arrayOf(intArrayOf(15, 0, 1, 0, 1, 1, 1, 1, 1, 0, 0, 1, 0, 1, 1),
+                                    intArrayOf(0, 14, 0, 0, 0, 0, 0, 0, 0, 0, 5, 0, 0, 0, 0),
+                                    intArrayOf(1, 0, 13, 0, 1, 1, 1, 1, 2, 0, 0, 4, 0, 1, 1),
+                                    intArrayOf(0, 0, 0, 12, 0, 0, 0, 0, 0, 1, 0, 0, 3, 0, 0),
+                                    intArrayOf(1, 0, 1, 0, 11, 4, 3, 2, 1, 0, 0, 1, 0, 2, 1),
+                                    intArrayOf(1, 0, 1, 0, 4, 10, 3, 2, 1, 0, 0, 1, 0, 2, 1),
+                                    intArrayOf(1, 0, 1, 0, 3, 3, 9, 2, 1, 0, 0, 1, 0, 2, 1),
+                                    intArrayOf(1, 0, 1, 0, 2, 2, 2, 8, 1, 0, 0, 1, 0, 2, 1),
+                                    intArrayOf(1, 0, 2, 0, 1, 1, 1, 1, 7, 0, 0, 2, 0, 1, 1),
+                                    intArrayOf(0, 0, 0, 1, 0, 0, 0, 0, 0, 6, 0, 0, 1, 0, 0),
+                                    intArrayOf(0, 5, 0, 0, 0, 0, 0, 0, 0, 0, 5, 0, 0, 0, 0),
+                                    intArrayOf(1, 0, 4, 0, 1, 1, 1, 1, 2, 0, 0, 4, 0, 1, 1),
+                                    intArrayOf(0, 0, 0, 3, 0, 0, 0, 0, 0, 1, 0, 0, 3, 0, 0),
+                                    intArrayOf(1, 0, 1, 0, 2, 2, 2, 2, 1, 0, 0, 1, 0, 2, 1),
+                                    intArrayOf(1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 0, 1, 0, 1, 1)))),
+            )
+        }
     }
 
 
     @ParameterizedTest
     @MethodSource(value = ["pair"])
-    fun buildFromZTest(data: Pair<IntArray, String>) {
-        val prefix = FindTheStringWithLCP.buildFromZ(data.first)
-        assertEquals(data.second, prefix)
+    fun buildFromZTest(data: Triple<IntArray, Char, String>) {
+        val prefix = FindTheStringWithLCP.buildFromZ(data.first, data.second)
+        assertEquals(data.third, prefix)
     }
 
 
@@ -42,6 +94,13 @@ class FindTheStringWithLCPTest {
     fun zFunctionTest(data: Pair<String, IntArray>) {
         val prefix = FindTheStringWithLCP.zFunction(data.first)
         assertEquals(data.second.joinToString(), prefix.joinToString())
+    }
+
+    @ParameterizedTest
+    @MethodSource(value = ["findTheString"])
+    fun findTheStringTest(data: Pair<String, Array<IntArray>>) {
+        val s = FindTheStringWithLCP.findTheString(data.second)
+        assertEquals(data.first, s)
     }
 
 }
